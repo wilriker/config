0a3ff53847486b4430c58ea982cc181ca8b299e7
Manuel Coenen <mcoenen@vwd.com>
2016-06-07 13:25:34 +0200

    Improve herbstclient completions

--- fish/completions/herbstclient.fish
+++ fish/completions/herbstclient.fish
@@ -1,7 +1,16 @@
-set -l alphabet a b c d e f g h i j k l m n o p q r s t u v w x y z
-for letter in $alphabet
-    set -l completions (herbstclient -q complete_shell 0 $letter)
-    for completion in $completions
-        complete -xc herbstclient -d "Command" -a $completion
+function get_herbstclient_completion
+    set -l cmdline (commandline)
+    set -l tokens (commandline -o)
+    set -l parameters
+    if test (count $tokens) -gt 1
+        set parameters $tokens[2..-1]
     end
+    set -l position (math (count $parameters) - 1)
+    set -l completions (herbstclient -q complete $position $parameters)
+    if string match $completions[1] $parameters[-1]
+        set position (math $position + 1)
+    end
+    herbstclient -q complete $position $parameters
 end
+
+complete -xc herbstclient -d "" -a '(get_herbstclient_completion)'
