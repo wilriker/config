01bb0e3c62c08a594ae9ef455bbf547d0c60f44a
Manuel Coenen <manuel.coenen@gmail.com>
2016-06-11 17:02:13 +0200

    Fallback to ping -6 if ping6 does not exist (anymore)

--- fish/functions/ping6.fish
+++ fish/functions/ping6.fish
@@ -1,3 +1,8 @@
 function ping6
+    if not exists --on-disk-only ping6 >&- ^&-
+        echo "ping6 not found, falling back to ping -6"
+        ping -6 $argv
+        return
+    end
 	exists grc; and grc -es --colour=auto ping6 $argv; or command ping6 $argv
 end
