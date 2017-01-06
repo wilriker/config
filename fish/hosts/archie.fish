64c19b3f75f7ff854518812b27bfdef7d102c8dd
Manuel Coenen <manuel.coenen@gmail.com>
2017-01-06 17:52:59 +0100

    Add clojure home

--- fish/hosts/archie.fish
+++ fish/hosts/archie.fish
@@ -4,4 +4,5 @@ if status --is-login
 	set -gx M2_HOME /opt/maven
 	set -gx GRADLE_HOME /usr/share/java/gradle
     set -gx SANE_CONFIG_DIR $HOME/.config/sane.d
+    set -gx CLOJURE_HOME /usr/share/clojure
 end
