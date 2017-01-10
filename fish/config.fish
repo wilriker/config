fd18d220254ed65bd2fef191498689634021a33a
Manuel Coenen <mcoenen@vwd.com>
2017-01-10 17:13:57 +0100

    Set tmux TERM to more sensible value

--- fish/config.fish
+++ fish/config.fish
@@ -30,21 +30,23 @@ if status --is-interactive
 
         # Fix DEL key in st
         case 'st*'
-            begin
-                tput smkx ^/dev/null
-                function fish_enable_keypad_transmit --on-event fish_postexec
-                    tput smkx ^/dev/null
-                end
-
-                function fish_disable_keypad_transmit --on-event fish_preexec
-                    tput rmkx ^/dev/null
-                end
-            end
+            set -gx is_simple_terminal 1
 
         case "linux"
             function fish_prompt
                 fish_fallback_prompt
             end
+    end
+
+    if set -q is_simple_terminal
+        tput smkx ^/dev/null
+        function fish_enable_keypad_transmit --on-event fish_postexec
+            tput smkx ^/dev/null
+        end
+
+        function fish_disable_keypad_transmit --on-event fish_preexec
+            tput rmkx ^/dev/null
+        end
     end
 end
 
