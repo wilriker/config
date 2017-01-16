9312a7ee890cc93d759ae9c94d5b81f1cee1d6bb
Manuel Coenen <mcoenen@vwd.com>
2017-01-16 14:13:28 +0100

    Move protection of subdirs in $HOME into machine local variable

--- fish/functions/rmthis.fish
+++ fish/functions/rmthis.fish
@@ -1,9 +1,14 @@
 function rmthis --description 'Remove the current directory with all its contents'
-    set -l DIR (pwd)
-    if string match -riq -- '^'$HOME'(/(Downloads|Dropbox|tmp)?)?$' $DIR
+    set -l protected_subdirs_pattern
+    if set -q protected_home_subdirs; and test (count $protected_home_subdirs) -gt 0
+        set protected_subdirs_pattern '(?:/('(string join '|' $protected_home_subdirs)')?)?'
+    end
+    set -l protected_dirs_pattern (string join '' '^' $HOME $protected_subdirs_pattern '$')
+    set -l dir (pwd)
+    if string match -rq -- $protected_subdirs_pattern $dir
         echo "Do NOT try to delete important directories!" >&2
         return 1
     end
     cd ..
-    command rm -rf -- $DIR
+    command rm -rf -- $dir
 end
