8438db067e66cb9bfa302ff274b4fb4663d69a16
Manuel Coenen <mcoenen@vwd.com>
2017-01-16 13:07:23 +0100

    Protect some more directories

--- fish/functions/rmthis.fish
+++ fish/functions/rmthis.fish
@@ -1,7 +1,7 @@
 function rmthis --description 'Remove the current directory with all its contents'
-	set -l DIR (pwd)
-    if string match -q -- $HOME $DIR
-        echo "Do NOT try to delete \$HOME!" >&2
+    set -l DIR (pwd)
+    if string match -riq -- '^'$HOME'(/(Downloads|Dropbox|tmp)?)?$' $DIR
+        echo "Do NOT try to delete important directories!" >&2
         return 1
     end
     cd ..
