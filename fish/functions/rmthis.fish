dc5b7e94bbc3fdb80af68a2d211a23a6d4caa616
Manuel Coenen <mcoenen@vwd.com>
2017-01-16 10:18:46 +0100

    Do NOT delete $HOME

--- fish/functions/rmthis.fish
+++ fish/functions/rmthis.fish
@@ -1,5 +1,9 @@
 function rmthis --description 'Remove the current directory with all its contents'
 	set -l DIR (pwd)
+    if string match -q -- $HOME $DIR
+        echo "Do NOT try to delete \$HOME!" >&2
+        return 1
+    end
     cd ..
     command rm -rf -- $DIR
 end
