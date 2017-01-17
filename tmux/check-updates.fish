eb8e1727915387ab3aaff6d67bcaee6eeffd010c
Manuel Coenen <mcoenen@vwd.com>
2017-01-17 11:30:28 +0100

    Move update check script here as it belongs to tmux

---
+++ tmux/check-updates.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+set updates 0
+
+if type -q pacaur
+    set updates (pacaur -Qu | wc -l)
+else if type -q pacman
+    set updates (pacman -Qu | wc -l)
+else if type -q yum
+    set -l update_check_file /tmp/last-yum-update-check
+    set -l update_result_file /tmp/yum-update-count
+    set -l lock_file /tmp/yum-update-check.lock
+    if not test -f $update_check_file
+        echo 0 > $update_check_file
+    end
+    set -l now (date +%s)
+    set -l old (cat $update_check_file)
+    set -l diff (math "$now - $old")
+    if test $diff -gt 3600
+        echo $now > $update_check_file
+        set updates (flock -xn $lock_file yum check-update | egrep "(\.i386|\.x86_64|\.noarch|\.src)" | wc -l)
+        echo $updates > $update_result_file
+    else
+        set updates (cat $update_result_file)
+    end
+end
+
+echo -n "$updates"
