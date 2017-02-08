f0789f2e2af30a5527ca271d07fdd140f107734e
Manuel Coenen <mcoenen@vwd.com>
2017-02-08 14:48:05 +0100

    Use separate functions when there is a corresponding nvim flag

--- fish/conf.d/nvim.fish
+++ fish/conf.d/nvim.fish
@@ -5,9 +5,16 @@ if status is-login
         exit
     end
 
-    for exec in edit ex rview rvim vedit vi view vim
+    for exec in edit vi vim
         echo "function $exec --wraps $exec; nvim \$argv; end" | source
     end
+
+    echo "function ex --wraps ex; nvim -e \$argv; end" | source
+
+    echo "function view --wraps view; nvim -R \$argv; end" | source
+    echo "function rview --wraps rview; nvim -RZ \$argv; end" | source
+
+    echo "function rvim --wraps rvim; nvim -Z \$argv; end" | source
 
     echo "function vimdiff --wraps vimdiff; nvim -d \$argv; end" | source
 end
