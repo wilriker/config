b70337d5d0fef58735a25e70d3518dc3d1d3d97d
Manuel Coenen <mcoenen@vwd.com>
2017-05-19 09:06:34 +0200

    Add case where feh is called without args

--- fish/functions/feh.fish
+++ fish/functions/feh.fish
@@ -1,5 +1,7 @@
 function feh
-	if string match -qr -- '-.*' $argv[1]
+	if test (count $argv) -eq 0
+        command feh
+    else if string match -qr -- '-.*' $argv[1]
         command feh $argv
     else
         set -l file (realpath $argv[1])
