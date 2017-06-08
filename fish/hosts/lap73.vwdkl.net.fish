25705b37f10b540ba987a4c802d2b5aa5f0d5e08
Manuel Coenen <mcoenen@vwd.com>
2017-06-08 15:58:25 +0200

    Changes related to new CentOS version

--- fish/hosts/lap73.vwdkl.net.fish
+++ fish/hosts/lap73.vwdkl.net.fish
@@ -3,13 +3,13 @@
 #if status is-login
 
     # $PROG_HOME dirs
-    set -gx JAVA_HOME /export/home/mcoenen/opt/jdk1.8
-    set -gx GRADLE_HOME /export/home/mcoenen/opt/gradle
-    set -gx M2_HOME /export/home/mcoenen/opt/maven
+    set -gx JAVA_HOME $HOME/opt/jdk1.8
+    set -gx GRADLE_HOME $HOME/opt/gradle
+    set -gx M2_HOME $HOME/opt/maven
     set -gx WORKON_HOME ~/.virtualenvs
-    set -gx NODE_HOME /export/home/mcoenen/opt/node
-    set -gx GOROOT /export/home/mcoenen/opt/go
-    set -gx GOPATH /export/home/mcoenen/workspace/go
+    set -gx NODE_HOME $HOME/opt/node
+    set -gx GOROOT $HOME/opt/go
+    set -gx GOPATH $HOME/workspace/go
 
     set -gx SHELL (command -s fish)
 #end
