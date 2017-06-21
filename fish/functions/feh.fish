6f77a6c4426080e7657b8aed6610fd2e5bc5775c
Manuel Coenen <mcoenen@vwd.com>
2017-06-21 13:31:39 +0200

    Better detection of feh parameters

--- fish/functions/feh.fish
+++ fish/functions/feh.fish
@@ -1,7 +1,7 @@
 function feh
 	if test (count $argv) -eq 0
         command feh
-    else if string match -qr -- '-.*' $argv[1]
+    else if string match -qr -- '^-.*' (string trim -- argv[1])
         command feh $argv
     else
         set -l file (realpath $argv[1])
