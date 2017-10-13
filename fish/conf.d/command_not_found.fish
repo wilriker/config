8355a77e0843c92b19db951547cd0bd7d8652be3
Manuel Coenen <mcoenen@vwd.com>
2017-10-13 09:09:09 +0200

    Move yum command-not-found-handler to conf.d

---
+++ fish/conf.d/command_not_found.fish
@@ -0,0 +1,19 @@
+if not status is-interactive
+    return
+end
+
+if type -q __fish_command_not_found_handler
+    return
+end
+
+if type -p -q yum
+    function __fish_command_not_found_handler --on-event fish_command_not_found
+        set -l __packages (yum whatprovides "*bin/$argv[1]" | egrep "(\.i386|\.x86_64|\.noarch|\.src)" | sort -u)
+        if count $__packages >/dev/null
+            printf "%s may be found in the following packages:\n" "$argv[1]"
+            printf "  %s\n" $__packages
+        else
+            __fish_default_command_not_found_handler $argv[1]
+        end
+    end
+end
--- fish/config.fish
+++ fish/config.fish
@@ -9,18 +9,6 @@ else
     touch $host_specific_file
 end
 
-if status is-interactive; and type -p -q yum
-    function __fish_command_not_found_handler --on-event fish_command_not_found
-        set -l __packages (yum whatprovides "*bin/$argv[1]" | egrep "(\.i386|\.x86_64|\.noarch|\.src)" | sort -u)
-        if count $__packages >/dev/null
-            printf "%s may be found in the following packages:\n" "$argv[1]"
-            printf "  %s\n" $__packages
-        else
-            __fish_default_command_not_found_handler $argv[1]
-        end
-    end
-end
-
 # Make PATH distinct
 vardistinct PATH
 
