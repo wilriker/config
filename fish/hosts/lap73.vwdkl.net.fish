64defee43a1a996395bb1d0298beccb6ac28ecbe
Manuel Coenen <mcoenen@vwd.com>
2017-10-19 10:09:47 +0200

    Set new JAVA_HOME

--- fish/hosts/lap73.vwdkl.net.fish
+++ fish/hosts/lap73.vwdkl.net.fish
@@ -3,7 +3,7 @@
 #if status is-login
 
     # $PROG_HOME dirs
-    set -gx JAVA_HOME $HOME/opt/jdk1.8
+    set -gx JAVA_HOME $HOME/opt/jdk9
     set -gx GRADLE_HOME $HOME/opt/gradle
     set -gx M2_HOME $HOME/opt/maven
     set -gx WORKON_HOME ~/.virtualenvs
