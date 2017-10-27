13fe23ddb408cfbfd110f698e28258b1b8e0137d
Manuel Coenen <manuel.coenen@gmail.com>
2017-10-27 12:54:43 +0200

    Add function to delete orphaned sidecar files

---
+++ fish/functions/remove-orphaned-sidecar-files.fish
@@ -0,0 +1,22 @@
+# Defined in /tmp/fish.KgfyiV/remove-orphaned-sidecar-files.fish @ line 2
+function remove-orphaned-sidecar-files --description 'Remove sidecar files that have no original image present recursively from the current directory' --argument extension print
+	if test -z $extension
+        echo "Usage: remove-orphaned-sidecar-files <sidecar-extension> [--print-only]"
+        return 1
+    end
+
+    for sidecar in **.$extension
+        if not test -f (string replace ".$extension" "" $sidecar)
+            echo -n "Found orphaned $sidecar"
+
+            # If no print-only is given delete the file
+            if test -z $print
+                rm $sidecar
+                echo " - deleted"
+            else
+                # Finish print line
+                echo
+            end
+        end
+    end
+end
