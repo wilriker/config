3876e5272789800b2ba17c88f5c44f4d69a3f3ce
Manuel Coenen <mcoenen@vwd.com>
2017-12-06 09:30:19 +0100

    Add xrandr settings for out-of-docking-station, reset JAVA_HOME to Java 8 and make yumu more verbose again

--- fish/functions/yumu.fish
+++ fish/functions/yumu.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.c6GdS9/yumu.fish @ line 2
+# Defined in /home/mcoenen/.config/fish/functions/yumu.fish @ line 2
 function yumu
 	sudo yum -q update
     and echo 0 >/tmp/last-update-check
