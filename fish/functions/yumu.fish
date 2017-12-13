c356a3653c6adb4b7e3f35fa0fa723e198649e04
Manuel Coenen <mcoenen@vwd.com>
2017-12-13 09:54:42 +0100

    Make yum updates verbose again

--- fish/functions/yumu.fish
+++ fish/functions/yumu.fish
@@ -1,5 +1,5 @@
-# Defined in /home/mcoenen/.config/fish/functions/yumu.fish @ line 2
+# Defined in /tmp/fish.owxLAV/yumu.fish @ line 2
 function yumu
-	sudo yum -q update
+	sudo yum update
     and echo 0 >/tmp/last-update-check
 end
