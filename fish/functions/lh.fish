c621ae262e051591c4a4e5e282340559a6d674cc
Manuel Coenen <mcoenen@vwd.com>
2018-01-19 13:13:33 +0100

    Add support for exa as ls replacement

--- fish/functions/lh.fish
+++ fish/functions/lh.fish
@@ -1,3 +1,7 @@
-function lh --wraps=ls
-    ls -lah --group-directories-first $argv
+function lh
+    set -l params -lah --group-directories-first
+    if command -sq exa
+        set params $params -b -g --git
+    end
+    ls $params $argv
 end
--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,9 +1,11 @@
 function ls --description 'List contents of directory'
     set -l param
     if isatty 1
-        set param --color --classify
+        set param --color=always --classify
     end
-    if command -sq grc
+    if command -sq exa
+        command exa $param $argv
+    else if command -sq grc
         grc -es --colour=auto ls $param $argv
     else
         command ls $param $argv
