50cef459fa3d39eff7d180ee7b0880892e43c827
Manuel Coenen <manuel.coenen@gmail.com>
2018-02-08 09:59:30 +0100

    Add a way to not autostart X server and enable it on RasPis

--- fish/config.fish
+++ fish/config.fish
@@ -19,7 +19,7 @@ status is-interactive; or exit
 status is-login; or exit
 
 # startx
-if test -z "$DISPLAY" -a -z "$SSH_CONNECTION"
+if test -z "$DISPLAY" -a -z "$SSH_CONNECTION" -a -z "$NO_STARTX"
     if not command env DISPLAY=:0 xset -q >/dev/null ^&1
         sleep 1
         startx
---
+++ fish/hosts/raspi-dev.fish
@@ -0,0 +1,1 @@
+set -gx NO_STARTX true
