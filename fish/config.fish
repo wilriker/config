302546a1a0c4fe0434ccc347fd794f5d8f865a0e
Manuel Coenen <manuel.coenen@gmail.com>
2018-02-08 09:28:19 +0100

    Automatically start X 1 second after login

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,12 @@
+### Note: All scripts from conf.d dir have already been sourced at this point
+
 # Per-Host settings
 set -l host (hostname)
 set -l host_specific_file ~/.config/fish/hosts/$host.fish
 if test -f $host_specific_file
     source $host_specific_file
 else
-    echo Creating host specific config file: $host_specific_file
+    echo "Creating host specific config file: $host_specific_file"
     mkdir -p (dirname $host_specific_file)
     touch $host_specific_file
 end
@@ -12,3 +14,17 @@ end
 # Make PATH distinct
 vardistinct PATH
 
+# Every after here is only relevant for interactive shells
+status is-interactive; or exit
+
+# startx
+if test -z "$SSH_CONNECTION"; and not command env DISPLAY=:0 xset -q >/dev/null ^&1
+    sleep 1
+    startx
+    # exit
+end
+
+# tmux
+# test (whoami) = "root"; and exit
+# test -z "$TMUX"; or exit
+# tmx
