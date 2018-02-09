6d4fae8aef776482319e7ae1955b9369ddfac23b
Manuel Coenen <manuel.coenen@gmail.com>
2018-02-09 21:10:50 +0100

    Sleep shorter before starting X

--- fish/autostart/00-X.fish
+++ fish/autostart/00-X.fish
@@ -6,7 +6,7 @@ or exit
 # startx
 if test -z "$DISPLAY" -a -z "$SSH_CONNECTION" -a -z "$NO_STARTX"
     if not command env DISPLAY=:0 xset -q >/dev/null ^&1
-        sleep 1
+        sleep 0.5
         startx
         # exit
     end
