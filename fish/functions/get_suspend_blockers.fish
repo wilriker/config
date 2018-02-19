1522a28c8c0ea4a03fda7e51110ffe1c14063fe9
Manuel Coenen <manuel.coenen@gmail.com>
2018-02-19 09:29:48 +0100

    Simplify get_suspend_blockers and use sts from hlwm to sent so suspend

--- fish/functions/get_suspend_blockers.fish
+++ fish/functions/get_suspend_blockers.fish
@@ -1,18 +1,4 @@
-# Defined in /tmp/fish.3aJhQO/get_suspend_blockers.fish @ line 2
+# Defined in /tmp/fish.EtDa9F/get_suspend_blockers.fish @ line 1
 function get_suspend_blockers
-	set -l inhibitors (systemd-inhibit)
-    if test (count $inhibitors) -gt 1
-        set inhibitors $inhibitors[1..-3]
-        set -l blockers
-        while count $inhibitors >/dev/null ^&1
-            if string match -iq -- '*sleep*' $inhibitors[2]
-                and string match -iq -- '*block*' $inhibitors[4]
-                set blockers $blockers $inhibitors[1..5]
-            end
-            set -e inhibitors[1..5]
-        end
-        if test (count $blockers) -gt 0
-            echo -e (string join -- '\n' $blockers)
-        end
-    end
+	systemd-inhibit | fgrep -B3 "Mode: block" | grep -B1 -A2 "What:.*sleep" | egrep "(Who:|Why:)"
 end
--- fish/functions/sts.fish
+++ fish/functions/sts.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.QuNfHA/sts.fish @ line 2
+# Defined in /tmp/fish.FoH6UH/sts.fish @ line 2
 function sts --argument logout
 	set -l blockers (get_suspend_blockers)
     if count $blockers >/dev/null ^&1
-        if isatty
+        if status is-interactive
             echo "The following process(es) block suspend:"
             echo -e (string join '\n' $blockers)
         else
@@ -10,7 +10,7 @@ function sts --argument logout
         end
         while test (count (get_suspend_blockers)) -gt 0
             sleep 2
-            isatty; and echo -n "."
+            status is-interactive; and echo -n "."
         end
     end
     fish -c "sleep 2; sudo systemctl suspend &" &; disown
